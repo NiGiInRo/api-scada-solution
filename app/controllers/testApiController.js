d78809cf36292e978807efb3c77ef26487b8a6c3
NiGiInRo <nicolas.infante-rodriguez@siemens.com>
2023-06-12 19:33:00 -0500

    setting up deepblue

--- app/controllers/testApiController.js
+++ app/controllers/testApiController.js
@@ -1,4 +1,5 @@
-const { getJoke } = require("../services/testApiService");
+const { getJoke, getCameraInfo, getDeepBlueInfo, getCookieDeepblue } = require("../services/testApiService");
+const logger = require('../../logger');
 
 const getjokesController = async (req, res, next) => {
   try {
@@ -9,4 +10,29 @@ const getjokesController = async (req, res, next) => {
     next(err);
   }
 };
-module.exports = { getjokesController };
+
+const getCameraInfoController = async (req, res, next) => {
+  try {
+    const cameraInfo = await getCameraInfo();
+    console.log(`DEEPBLUE CONTROLLER: `);
+    console.log(req);
+    res.status(201).send(cameraInfo);
+  } catch (err) {
+    logger.info(err);
+    next(err);
+  }
+};
+
+const getDeepBlueInfoController = async (req, res, next) => {
+  try {
+    const cookie = await getCookieDeepblue();
+    console.log(`DEEPBLUE COOKIE: `);
+    console.log(cookie);
+    const deepblueInfo = await getDeepBlueInfo(cookie);
+    res.status(200).send(deepblueInfo);
+  } catch (err) {
+    logger.info(err);
+    next(err);
+  }
+};
+module.exports = { getjokesController, getCameraInfoController, getDeepBlueInfoController };
--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,6 @@
-const { getjokesController } = require('./controllers/testApiController')
+const { getjokesController, getCameraInfoController, getDeepBlueInfoController} = require('./controllers/testApiController')
 exports.init = app => {
     app.get('/geek', getjokesController)
+    app.post('/camera', getCameraInfoController);
+    app.get('/depblue', getDeepBlueInfoController);
 };
